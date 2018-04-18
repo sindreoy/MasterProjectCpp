4703894651a8a9ac4bfd05d4d623fda0fd68d61d
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2018-04-18 14:14:48 +0200

    CMake stuff

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -49,6 +49,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/testLogNormalInitialCondition.dir/DependInfo.cmake"
   "CMakeFiles/testSolution.dir/DependInfo.cmake"
   "CMakeFiles/main.dir/DependInfo.cmake"
   )
